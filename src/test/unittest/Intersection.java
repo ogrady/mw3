95e9fca164cf0d1b8e8e925bfba0253cce676021
Daniel <danielogrady@gmx.de>
2015-04-28 07:23:43 +0200

    more elaborated unit-test for intersections

--- src/test/unittest/Intersection.java
+++ src/test/unittest/Intersection.java
@@ -1,6 +1,5 @@
 package test.unittest;
 
-
 import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
@@ -13,6 +12,7 @@ import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
 public class Intersection {
+	private Shape[] shapes;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -24,6 +24,11 @@ public class Intersection {
 
 	@Before
 	public void setUp() throws Exception {
+		shapes = new Shape[4];
+		shapes[0] = new Rectangle(2, 2, 4, 4);
+		shapes[1] = new Rectangle(2, 8, 2, 4);
+		shapes[2] = new Rectangle(5, 9, 4, 4);
+		shapes[3] = new Rectangle(5, 5, 4, 2);
 	}
 
 	@After
@@ -31,23 +36,36 @@ public class Intersection {
 	}
 
 	@Test
-	public void test() {
+	public void testPath() {
 		final Path p = new Path(544, 384);
-		p.lineTo(544, 399);
-		p.lineTo(559, 399);
-		p.lineTo(559, 399);
-		p.lineTo(559, 384);
+		p.lineTo(5, 3);
+		p.lineTo(6, 10);
+		p.lineTo(7, 11);
+		p.lineTo(9, 3);
 		p.close();
-		final Shape[] s = new Shape[4];
-		s[0] = new Rectangle(0, 0, 800, 560);
-		s[1] = new Rectangle(800, 0, 800, 560);
-		s[2] = new Rectangle(0, 560, 800, 560);
-		s[3] = new Rectangle(800, 560, 800, 560);
+		assertEquals(true, intersectsWithAll(p));
+	}
+
+	@Test
+	public void testUnion() {
+		Rectangle r1, r2;
+		r1 = new Rectangle(4, 6, 2, 2);
+		r2 = new Rectangle(5, 7, 5, 2);
+		final Shape union = r1.union(r2)[0];
+		assertEquals(true, intersectsWithAll(union));
+	}
+
+	public void testRectangle() {
+		assertEquals(true, intersectsWithAll(new Rectangle(4, 3, 10, 8)));
+	}
+
+	private boolean intersectsWithAll(final Shape s) {
 		boolean intersects = false;
-		for (final Shape shape : s) {
-			intersects = shape.contains(p) || intersects;
+		for (final Shape shape : shapes) {
+			// intersects = shape.contains(p) || intersects;
+			intersects = shape.intersects(s) || intersects;
 		}
-		assertEquals(true, intersects);
+		return intersects;
 	}
 
 }
